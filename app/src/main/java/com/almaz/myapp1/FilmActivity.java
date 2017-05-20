aa4a905cf9927bfad250597c3044cb0520cf1cac
almazych <jerubro@ya.ru>
2017-05-20 12:46:22 +0300

    Перенес запрос конкретного фильма во второе активити

--- app/src/main/java/com/almaz/myapp1/FilmActivity.java
+++ app/src/main/java/com/almaz/myapp1/FilmActivity.java
@@ -1,29 +1,36 @@
 package com.almaz.myapp1;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.almaz.myapp1.MainActivity.API_KEY;
 
 public class FilmActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ID = "com.almaz.myapp1.id_film";
 
-    public static final String EXTRA_TITLE = "com.almaz.myapp1.title_film";
-    public static final String EXTRA_IMAGE = "com.almaz.myapp1.image_film";
-    public static final String EXTRA_OVERVIEW = "com.almaz.myapp1.overview_film";
+    @BindView(R.id.film_name)
+    TextView mTitle;
+    @BindView(R.id.film_image)
+    ImageView mImage;
+    @BindView(R.id.overview_film)
+    TextView mOverView;
 
+    Film film;
 
-
-    @BindView(R.id.film_name) TextView mTitle;
-    @BindView(R.id.film_image) ImageView mImage;
-    @BindView(R.id.overview_film) TextView mOverView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +38,32 @@ public class FilmActivity extends AppCompatActivity {
         setContentView(R.layout.film_item);
         ButterKnife.bind(this);
 
-        mTitle.setText(getIntent().getStringExtra(EXTRA_TITLE));
-        mOverView.setText(getIntent().getStringExtra(EXTRA_OVERVIEW));
-        Picasso.with(FilmActivity.this)
-                .load(String.format("https://image.tmdb.org/t/p/w185_and_h278_bestv2%s",getIntent().getStringExtra(EXTRA_IMAGE)))
-                .placeholder(R.drawable.poster_fon)
-                .into(mImage);
+        App.getApi().getFilm(getIntent().getStringExtra(EXTRA_ID), API_KEY, "ru-US").enqueue(new Callback<Film>() {
+            @Override
+            public void onResponse(Call<Film> call, Response<Film> response) {
+                if (response.isSuccessful()) {
+                    Log.d("TAG", "Status Code = " + response.code());
+                    film = response.body();
+
+                    mTitle.setText(film.getTitle());
+                    mOverView.setText(film.getOverview());
+
+                    Picasso.with(FilmActivity.this)
+                            .load(String.format("https://image.tmdb.org/t/p/w185_and_h278_bestv2%s",film.getPosterPath()))
+                            .placeholder(R.drawable.poster_fon)
+                            .into(mImage);
+                } else {
+                    try {
+                        Log.d("TAG", response.errorBody().string());
+                    } catch (IOException ioe) {
+                        Log.d("TAG", ioe.getLocalizedMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Film> call, Throwable t) {}
+        });
 
     }
-
 }
--- app/src/main/java/com/almaz/myapp1/MainActivity.java
+++ app/src/main/java/com/almaz/myapp1/MainActivity.java
@@ -18,16 +18,13 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_TITLE = "com.almaz.myapp1.title_film";
-    public static final String EXTRA_OVERVIEW = "com.almaz.myapp1.overview_film";
-    public static final String EXTRA_IMAGE = "com.almaz.myapp1.image_film";
+    public static final String EXTRA_ID = "com.almaz.myapp1.id_film";
     public static final String API_KEY = "1546eddf24e069a6848cd0c34766935f";
     public static final String TAG = "LogTag";
 
     RecyclerView recyclerView;
     PostsAdapter adapter;
     PostModel post;
-    Film film;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,31 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new PostsAdapter(new ArrayList<Result>(), new PostsAdapter.OnItemClickListener() {
             @Override public void onItemClick(final Result item) {
-                App.getApi().getFilm(item.getId(), API_KEY, "ru-US").enqueue(new Callback<Film>() {
-                    @Override
-                    public void onResponse(Call<Film> call, Response<Film> response) {
-                        if (response.isSuccessful()) {
-                            Log.d("TAG", "Status Code = " + response.code());
-                            film = response.body();
-
                             Intent i = new Intent(MainActivity.this, FilmActivity.class);
-                            i.putExtra(EXTRA_TITLE,item.getTitle());
-                            i.putExtra(EXTRA_OVERVIEW,item.getOverview());
-                            i.putExtra(EXTRA_IMAGE,item.getPosterPath());
+                            i.putExtra(EXTRA_ID,item.getId());
                             startActivity(i);
-
-                        } else {
-                            try {
-                                Log.d("TAG", response.errorBody().string());
-                            } catch (IOException ioe) {
-                                Log.d("TAG", ioe.getLocalizedMessage());
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<Film> call, Throwable t) {}
-                });
             }
         });
         recyclerView.setAdapter(adapter);
